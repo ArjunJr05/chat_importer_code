530dff2f5639b82031e7fd3eafe417087b00c7a0
ArjunJr05 <arjunfree256@gmail.com>
2026-02-24 12:40:05 +0530

    Add documentation site to docs/ folder for GitHub Pages

---
+++ docs/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.zoho.arattai"},{"l":"com.zoho.arattai.core"},{"l":"com.zoho.arattai.Message"}];updateSearchResults();
